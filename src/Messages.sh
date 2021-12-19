cd515639f6eb64e01efb8774565f8ace29f77616
Devin Lin <espidev@gmail.com>
2021-12-19 17:48:57 -0500

    Make REUSE compliant and add check to ci

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
+
+# SPDX-FileCopyrightText: 2020 Yuri Chornoivan
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 $XGETTEXT `find -name \*.cpp -o -name \*.qml` -o $podir/krecorder.pot
--- src/contents/ui/AboutPage.qml
+++ src/contents/ui/AboutPage.qml
@@ -1,7 +1,7 @@
 /*
  * Copyright 2021 Devin Lin <espidev@gmail.com>
  *
- * SPDX-License-Identifier: GPL-2.0-or-later
+ * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
 import QtQuick 2.15
